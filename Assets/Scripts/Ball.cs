c33259b31c7bdcc90a7a47f6c15fc6e6c5fca3b6
luckywjc2008 <550970259@qq.com>
2019-08-23 11:27:14 +0800

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Tag_Hole")
+        {
+            Destroy(this.gameObject,0.5f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/BallMoving.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMoving : MonoBehaviour
+{
+    [SerializeField]
+    public float floSpeed = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float floX = Input.GetAxis("Horizontal");
+	    float floY = Input.GetAxis("Vertical");
+	    if (floX > 0)
+	    {
+	        gameObject.GetComponent<Rigidbody>().velocity = Vector3.right * floSpeed;
+	    }
+	    else if(floX < 0)
+	    {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.left * floSpeed;
+        }
+
+	    if (floY > 0)
+	    {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.forward * floSpeed;
+        }
+	    else if(floY < 0)
+	    {
+            gameObject.GetComponent<Rigidbody>().velocity = Vector3.back * floSpeed;
+        }
+	}
+}
